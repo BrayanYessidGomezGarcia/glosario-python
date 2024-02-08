aac41bffc043974d3f73d6cff5e3c62a75a3248f
BrayanYessidGomezGarcia <Bryanyessid1409@gmail.com>
2024-02-08 12:37:34 -0500

    feat: :construction: Estoy agregando las notas de las tareas, proyecto y filtro

--- asignarNotas.py
+++ asignarNotas.py
@@ -1,6 +1,7 @@
 import listasCampus
 import Campers
 import mensajeError
+import os
 
 def NotasAsignadas():
     mostrar_campers= listasCampus.cargarCampers()
@@ -39,10 +40,46 @@ def NotasAsignadas():
         else:
             print("Reprobado")
 
+
         camper_seleccionado['NotaTeorica'] = nota_teorica
         camper_seleccionado['NotaPractica'] = nota_practica
 
         listasCampus.guardarCampers(mostrar_campers)    
 
 
+def agregarnotas():
+    while True:
+        try:
+            print("**AGREGAR NOTAS**")
+            print("1. Notas Actividades")
+            print("2. Notas filtro")
+            print("3. Notas Proyecto")
+            op = int(input("\t>>Escoja una opcion (1-3): "))
+            if op < 1 or op > 5:
+                    mensajeError.msgError("Error. Opcion Invalida (de 1 a 5)")
+                    continue
+            return op
+        
+        except ValueError:
+            mensajeError.msgError("Error. Opcion Invalida (de 1 a 5)")
+            continue
+
+def notasActividades():
+    os.system("cls") 
+    while True:
+        op= agregarnotas()
+        if op == 1:
+            os.system("cls")
+            for i in range(3):
+                notas= float(input("Ingrese las notas de las actividades: "))
+                while not notas:
+                    mensajeError.msgError("Error de notas ")
+                    notas= input("Ingrese las notas de las actividades: ")
+
+                
+
+        
+        
+
+    
 
